77c9de57b38a739470a3e0056ae90cc5135ac5fd
Rémi Bardon <remi@remibardon.name>
2024-08-27 22:00:14 +0200

    ✨ Allow passing redirects (e.g. `2>&1`) as argument to `edo` and `traced`

--- edo.sh
+++ edo.sh
@@ -1,3 +1,8 @@
+# Allows passing a pipe as argument.
+REGEX_ALLOW_PIPES='s#\\|#|#'
+# Allows passing redirects (e.g. `2>&1`) as argument.
+REGEX_ALLOW_REDIRECTS='s#([[:digit:]]+)\\>\\&([[:digit:]]+)#\1>\&\2#'
+
 # Inspired by [Paludis's `edo` function](https://gitlab.exherbo.org/paludis/paludis/-/blob/ac9f4552b0a1ef1ed7c43071cf0846e35b99f4ea/paludis/repositories/e/ebuild/exheres-0/build_functions.bash#L247-251),
 # with additional dry run logic.
 edo() {
@@ -5,9 +10,8 @@ edo() {
 		dry_run $@
 	else
 		trace $@
-		# NOTE: `eval $@` would break spaces in arguments.
-		# NOTE: `sed 's/\\|/|/'` allows passing a pipe as argument.
-		eval $(printf '%q ' "$@" | sed 's/\\|/|/')
+		# NOTE: `$@`, `"$@"` or `eval $@` would break spaces in arguments.
+		eval $(printf '%q ' "$@" | sed "$REGEX_ALLOW_PIPES" | sed -E "$REGEX_ALLOW_REDIRECTS")
 	fi
 	return $?
 }
@@ -15,7 +19,6 @@ edo() {
 # Like `edo` but with no dry run logic.
 traced() {
 	trace $@
-	# NOTE: `eval $@` would break spaces in arguments.
-	# NOTE: `sed 's/\\|/|/'` allows passing a pipe as argument.
-	eval $(printf '%q ' "$@" | sed 's/\\|/|/')
+	# NOTE: `$@`, `"$@"` or `eval $@` would break spaces in arguments.
+	eval $(printf '%q ' "$@" | sed "$REGEX_ALLOW_PIPES" | sed -E "$REGEX_ALLOW_REDIRECTS")
 }
