860a2b427e595e563e60a336ccac5ff1a972215a
Rémi Bardon <remi@remibardon.name>
2025-01-11 14:07:58 +0100

    fix: Fix `ask_yes_no`

--- yes_no.sh
+++ yes_no.sh
@@ -1,19 +1,27 @@
 ask_yes_no() {
-	question ${1:?}
-	local yes_no_default=${2:?} yes_no_answer
+	question "${1:?}"
+	local yes_no_default="${2:?}" yes_no_answer
 
-	printf "${LOGGER_MARGIN}${BCyan}Answer (y|N): "
+	choices() {
+		case "${yes_no_default}" in
+			y|Y) echo 'Y|n' ;;
+			n|N) echo 'y|N' ;;
+			*) echo 'y|n' ;;
+		esac
+	}
+
+	printf "${LOGGER_MARGIN}${BCyan}Answer ($(choices)): "
 	read -n 1 yes_no_answer
 	printf "${Color_Off}\n"
-	case "${yes_no_answer:-${yes_no_default}}" in
+	case "${yes_no_answer:-"${yes_no_default}"}" in
 		y|Y) return 0 ;;
 		n|N|*) return 1 ;;
 	esac
 }
 
 if_yes() {
-	local q=${1:?}
-	local yes_no_default=${2:?}
+	local q="${1:?}"
+	local yes_no_default="${2:?}"
 	shift 2
 	ask_yes_no "$q" "$yes_no_default" && edo $@
 }
