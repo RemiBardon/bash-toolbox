b110498b86927486db8310d6eed6babcc570f783
Rémi Bardon <remi@remibardon.name>
2025-01-22 21:45:16 +0100

    feat(log): Support multiline

--- log.sh
+++ log.sh
@@ -19,58 +19,82 @@ if (( $DRY_RUN )); then LOG_DRY_RUN=1; fi
 
 LOGGER_MARGIN="        "
 
-trace() {
+trace_() {
 	if (( $LOG_TRACE )); then
 		printf "${DPurple}[${Purple}TRACE${DPurple}]${Color_Off} $(format_secondary "$@")\n" >&2
 	fi
 }
-debug() {
+trace() {
+	echo "$@" | while read -r line; do trace_ "$line"; done
+}
+debug_() {
 	if (( $LOG_DEBUG )); then
 		printf "${DWhite}[${White}DEBUG${DWhite}]${Color_Off} ${Black}${On_Yellow}$(decolor <<< "$@")${Color_Off}\n" >&2
 	fi
 }
-info() {
+debug() {
+	echo "$@" | while read -r line; do debug_ "$line"; done
+}
+info_() {
 	if (( $LOG_INFO )); then
 		printf " ${DBlue}[${Blue}INFO${DBlue}]${Color_Off} $@\n" >&2
 	fi
 }
-warn() {
+info() {
+	echo "$@" | while read -r line; do info_ "$line"; done
+}
+warn_() {
 	if (( $LOG_WARN )); then
 		printf " ${DYellow}[${Yellow}WARN${DYellow}]${Color_Off} ${Yellow}$(decolor <<< "$@")${Color_Off}\n" >&2
 	fi
 }
-error() {
+warn() {
+	echo "$@" | while read -r line; do warn_ "$line"; done
+}
+error_() {
 	printf "${DRed}[${Red}ERROR${DRed}]${Color_Off} ${Red}$(decolor <<< "$@")${Color_Off}\n" >&2
 }
-success() {
+error() {
+	echo "$@" | while read -r line; do error_ "$line"; done
+}
+success_() {
 	printf "   ${DGreen}[${Green}OK${DGreen}]${Color_Off} $@\n" >&2
 }
-question() {
+success() {
+	echo "$@" | while read -r line; do success_ "$line"; done
+}
+question_() {
 	printf "  ${DCyan}[${Cyan}???${DCyan}]${Color_Off} $@\n" >&2
 }
-dry_run() {
+question() {
+	echo "$@" | while read -r line; do question_ "$line"; done
+}
+dry_run_() {
 	# NOTE: Remove leading underscore-prefixed functions, to avoid logging things like `log_as_info_`.
 	local command=$(echo "$@" | sed -E 's/^([a-zA-Z0-9_]*_ )+//')
 	if (( $LOG_DRY_RUN )); then
 		printf "${DWhite}[${White}DRY_RUN${DWhite}]${Color_Off} sh> $(format_secondary $command)\n" >&2
 	fi
 }
+dry_run() {
+	echo "$@" | while read -r line; do dry_run_ "$line"; done
+}
 
 log_as_trace_() {
-	( set -o pipefail; "$@" 2>&1 | while read -r line; do trace "$line"; done)
+	( set -o pipefail; "$@" 2>&1 | while read -r line; do trace_ "$line"; done )
 }
 log_as_debug_() {
-	( set -o pipefail; "$@" 2>&1 | while read -r line; do debug "$line"; done)
+	( set -o pipefail; "$@" 2>&1 | while read -r line; do debug_ "$line"; done )
 }
 log_as_info_() {
-	( set -o pipefail; "$@" 2>&1 | while read -r line; do info "$line"; done)
+	( set -o pipefail; "$@" 2>&1 | while read -r line; do info_ "$line"; done )
 }
 log_as_warn_() {
-	( set -o pipefail; "$@" 2>&1 | while read -r line; do warn "$line"; done)
+	( set -o pipefail; "$@" 2>&1 | while read -r line; do warn_ "$line"; done )
 }
 log_as_error_() {
-	( set -o pipefail; "$@" 2>&1 | while read -r line; do error "$line"; done)
+	( set -o pipefail; "$@" 2>&1 | while read -r line; do error_ "$line"; done )
 }
 log_as_success_() {
-	( set -o pipefail; "$@" 2>&1 | while read -r line; do success "$line"; done)
+	( set -o pipefail; "$@" 2>&1 | while read -r line; do success_ "$line"; done )
 }
